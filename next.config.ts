353d51f629232a961d287d1162ef9b52d3bebb6f
thomas-addamo <thomas.addamo2@jcmaxwell.it>
2026-03-25 19:51:52 +0100

    feat: fase 1 - scaffold + file JSON base

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      new URL("https://images.unsplash.com/**"),
+      new URL("https://res.cloudinary.com/**"),
+    ],
+  },
 };
 
 export default nextConfig;
