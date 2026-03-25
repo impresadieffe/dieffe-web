d7f3d845c9db65a578e92dce673d09dc8ed34215
thomas-addamo <thomas.addamo2@jcmaxwell.it>
2026-03-25 21:59:11 +0100

    feat: fase 2C - footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -26,6 +27,7 @@ export default function RootLayout({
       <body className={`${inter.className} min-h-full flex flex-col`}>
         <Navbar />
         <main className="flex-1 pt-20">{children}</main>
+        <Footer />
       </body>
     </html>
   );
